faed799777de25b625a5fca35cd2af6d03b41a5c
Caio Vinícius <86082269+caiovpsilveira@users.noreply.github.com>
2022-09-12 22:32:54 -0300

    Delete Processador8b directory

--- Processador8b/simulation/modelsim/rtl_work/processador_8_bits/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity processador_8_bits is
-    port(
-        clk             : in     vl_logic;
-        reset           : in     vl_logic
-    );
-end processador_8_bits;
--- Processador8b/simulation/modelsim/rtl_work/simulacao_processador/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity simulacao_processador is
-end simulacao_processador;
